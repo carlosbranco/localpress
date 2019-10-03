d37cb65ef942aaec389607aa16cc8f5b178fae2f
Carlos Branco <carlos@cria-web.com>
2019-10-03 18:51:35 +0700

    Basic Project Creation

---
+++ files-helper.php
@@ -0,0 +1,156 @@
+<?php
+function remove_accent($str)
+{
+    $a = array(
+        'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í',
+        'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß',
+        'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï',
+        'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'A', 'a', 'A',
+        'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'Ð', 'd', 'E',
+        'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G',
+        'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', '?',
+        '?', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', '?', '?', 'L', 'l', 'N',
+        'n', 'N', 'n', 'N', 'n', '?', 'O', 'o', 'O', 'o', 'O', 'o', 'Œ', 'œ', 'R', 'r',
+        'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'Š', 'š', 'T', 't', 'T', 't',
+        'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w',
+        'Y', 'y', 'Ÿ', 'Z', 'z', 'Z', 'z', 'Ž', 'ž', '?', 'ƒ', 'O', 'o', 'U', 'u', 'A',
+        'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', '?',
+        '?', '?', '?', '?', '?'
+    );
+    $b = array(
+        'A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I',
+        'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's',
+        'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i',
+        'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A',
+        'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E',
+        'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G',
+        'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ',
+        'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N',
+        'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r',
+        'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't',
+        'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w',
+        'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A',
+        'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A',
+        'a', 'AE', 'ae', 'O', 'o'
+    );
+    return str_replace($a, $b, $str);
+}
+
+function slugify($str)
+{
+    return strtolower(preg_replace(
+        array('/[^a-zA-Z0-9 -]/', '/[ -]+/', '/^-|-$/'),
+        array('', '-', ''),
+        remove_accent(name($str))
+    ));
+}
+
+function recurse_copy($src, $dst)
+{
+    shell_exec("cp -R $src $dst");
+    return true;
+    $dir = opendir($src);
+    @mkdir($dst);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
+}
+
+function getProjects()
+{
+    if (!file_exists(PROJECTS_FOLDER)) {
+        mkdir(PROJECTS_FOLDER, 0777, true);
+    }
+    if (!file_exists(TRASH_FOLDER)) {
+        mkdir(TRASH_FOLDER, 0777, true);
+    }
+    if (!file_exists(DATA_FOLDER)) {
+        mkdir(DATA_FOLDER, 0777, true);
+    }
+    $files = glob(PROJECTS_FOLDER . "/*.txt");
+    return $files;
+}
+
+function createProject()
+{
+    if (!isset($_POST['project_name'])) {
+        return false;
+    }
+
+    $name = $_POST['project_name'];
+    
+    if (file_exists(PROJECTS_FOLDER . '/' . basename($name) . '.txt')) {
+        define('ERROR', 'Already exists a project with the name you provide.');
+        return true;
+    }
+    //CREATE PROJECT BASE
+    file_put_contents(PROJECTS_FOLDER . '/' . basename($name) . '.txt', '');
+
+    //BACKUP DATA FROM CURRENT WORDPRESS
+    if ($_POST['action'] == 'current') {
+        mkdir(DATA_FOLDER . '/' . slugify( basename( $name )), 0777, true);
+        recurse_copy(THEMES_FOLDER, DATA_FOLDER . '/' . slugify( basename( $name )) . '/themes' );
+        recurse_copy(PLUGINS_FOLDER, DATA_FOLDER . '/' . slugify( basename( $name )) . '/plugins' );
+        file_put_contents(DATA_FOLDER . '/' . slugify( basename( $name )) . '/themes/project.txt', $name );
+    } else {
+        //BACKUP FROM OTHER PROJECT
+    }
+}
+
+function backupProject()
+{
+    # code...
+}
+
+function restoreProject()
+{
+    # code...
+}
+
+function isProjectEmpty($project_name)
+{
+    if( !file_exists( DATA_FOLDER . '/' . slugify( basename($project_name )) . '/plugins' ) ){
+        return true;
+    }
+
+    if( !file_exists( DATA_FOLDER . '/' . slugify( basename($project_name )) . '/themes' ) ){
+        return true;
+    }
+
+    return false;
+}
+
+function getBackupSize($project_name)
+{
+    if( !file_exists( DATA_FOLDER . '/' . slugify( basename($project_name )) )) {
+        return 0;
+    }
+
+    return format_size( foldersize(  DATA_FOLDER . '/' . slugify( basename($project_name ))  ));
+}
+
+function name($project_name)
+{
+    return str_replace('.txt','', $project_name);
+}
+
+function currentProject()
+{
+    if( !file_exists( THEMES_FOLDER . '/project.txt' ) ){
+        return "None...";
+    }
+
+    return file_get_contents(THEMES_FOLDER . '/project.txt');
+}
+
+function isCurrentProject($current_project, $project)
+{
+    return $current_project == name(basename($project));
+}
--- index.php
+++ index.php
@@ -1,18 +1,140 @@
 <?php
-define( 'PLUGINS_FOLDER' , __DIR__ . '/../wp-content/plugins');
-define( 'THEMES_FOLDER' , __DIR__ . '/../wp-content/themes');
+set_time_limit(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+define('PLUGINS_FOLDER', __DIR__ . '/../wp-content/plugins');
+define('THEMES_FOLDER', __DIR__ . '/../wp-content/themes');
+define('PROJECTS_FOLDER', __DIR__ . '/projects');
+define('DATA_FOLDER', __DIR__ . '/data');
+define('TRASH_FOLDER', __DIR__ . '/trash');
 
-if( !file_exists(PLUGINS_FOLDER) ){
+if (!file_exists(PLUGINS_FOLDER)) {
     die("PLUGINS FOLDER NOT FOUNT. THIS FOLDER SHOULD BE ON WORDPRESS ROOT FOLDER");
 }
 
-if( !file_exists(THEMES_FOLDER) ){
+if (!file_exists(THEMES_FOLDER)) {
     die("THEMES FOLDER NOT FOUNT. THIS FOLDER SHOULD BE ON WORDPRESS ROOT FOLDER");
 }
 
-if( !file_exists(__DIR__ . '/../wp-config.php') ){
+if (!file_exists(__DIR__ . '/../wp-config.php')) {
     die("wp-config.php file not found. THIS FOLDER SHOULD BE ON WORDPRESS ROOT FOLDER.");
 }
 require_once __DIR__ . '/../wp-config.php';
+require_once __DIR__ . '/files-helper.php';
 #require_once __DIR__ . '/myphp-backup.php';
-require_once __DIR__ . '/myphp-restore.php';
+#require_once __DIR__ . '/myphp-restore.php';
+createProject();
+$projects = getProjects();
+$current_project = currentProject();
+?>
+  <!doctype html>
+  <html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
+    <style>
+      .error {
+        background: #e0455e;
+        color: #fff;
+      }
+      
+      .warning {
+        background: #ffba00;
+        color: #121212;
+        border-color: #ffba00;
+      }
+      
+      .success {
+        background: #15CD72;
+        color: #fff;
+        padding: 5px;
+        border-radius: 5px;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div class="container">
+      Local<strong>Press</span>
+<hr>
+<form method="POST" action="index.php">
+<fieldset>
+<div class="row">
+<div class="column"> <label for="nameField">Project Name</label>
+<input required type="text" placeholder="Name..." id="nameField" name="project_name"></div>
+<div class="column">
+<label for="action">Based on</label>
+<select id="action" name="action">
+<option value="current">Current Wordpress State</option>
+<?php
+foreach ($projects as $project) {
+    if (isProjectEmpty($project)) {
+        continue;
+    }
+    ?>
+    <option value="<?php echo $project ?>">Project: <?php echo name(basename($project)) ?></option>
+<?php } ?>
+</select></div>
+<div class="column">
+<label for="test">&nbsp;</label><input class="button-primary" type="submit" value="Create New Project"></div>
+</div>
+</fieldset>
+</form>
+<hr>
+<?php if (empty($projects)) {  ?>
+    <blockquote class="warning">
+    <p><em>You don't have any project created. You can create a new project with the configuration you have right now in your Wordpress.</em></p>
+    </blockquote>
+<?php } ?>
+<?php if (defined('SUCCESS')) {  ?>
+    <blockquote class="warning">
+    <p><em><?php echo SUCCESS ?></em></p>
+    </blockquote>
+<?php } ?>
+<?php if (defined('ERROR')) {  ?>
+    <blockquote class="error">
+    <p><em><?php echo ERROR ?></em></p>
+    </blockquote>
+<?php } ?>
+<table>
+<thead>
+<tr>
+<th>Name</th>
+<th>Backup</th>
+<th>Restore</th>
+<th></th>
+</tr>
+</thead>
+<tbody>
+<?php foreach ($projects as $project) { ?>
+    <tr>
+    <td><span class="<?php echo isCurrentProject($current_project, $project) ? 'success' : ''; ?>"><?php echo name(basename($project)) ?></span></td>
+    <td>
+    <?php
+    if (isCurrentProject($current_project, $project)) {
+        echo '<button class="button button-outline">Backup Now</button>';
+    } ?>
+    </td>
+    <td><?php
+    if (!isProjectEmpty($project)) {
+        if (isCurrentProject($current_project, $project)) {
+            echo '<button class="button button-outline">Restore Backup</button>';
+        } else {
+            echo '<button class="button button-outline">Change to This Project</button>';
+        }
+    } else {
+        echo '<small>Empty</small>';
+    } ?></td>
+    <td><button class="button button-clear float-right">Delete</button></td>
+    </tr>
+<?php } ?>
+</tbody>
+</table>
+</div>
+</body>
+</html>
